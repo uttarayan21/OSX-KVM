035b2f3ef999d053b50874bf6b9b1250c6ff12ca
Jonas Heinrich <onny@project-insanity.org>
2023-01-06 19:14:47 +0100

    Add NixOS flake support

--- OpenCore-Boot.sh
+++ OpenCore-Boot.sh
@@ -18,10 +18,10 @@ MY_OPTIONS="+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
 # This script works for Big Sur, Catalina, Mojave, and High Sierra. Tested with
 # macOS 10.15.6, macOS 10.14.6, and macOS 10.13.6.
 
-ALLOCATED_RAM="4096" # MiB
+ALLOCATED_RAM="8192" # MiB
 CPU_SOCKETS="1"
-CPU_CORES="2"
-CPU_THREADS="4"
+CPU_CORES="4"
+CPU_THREADS="8"
 
 REPO_PATH="."
 OVMF_DIR="."
